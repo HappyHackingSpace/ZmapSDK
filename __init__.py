1bb989c35ff6682bb0ddfffdc43e3051a90ed95a
atiilla <9992685+atiilla@users.noreply.github.com>
2025-04-09 02:14:19 +0200

    first commit

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""
+ZMap SDK - Python SDK for the ZMap network scanner
+"""
+
+from .zmapsdk import ZMap, ZMapError, ZMapCommandError
+
+__version__ = "0.1.0"
+__all__ = ["ZMap", "ZMapError", "ZMapCommandError"] 
